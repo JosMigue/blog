b7db81346c33225e77a726c381e4f09f0b82654f
JosMigue <jojo_miguel20@hotmail.com>
2020-10-07 17:23:51 -0500

    New login design by tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -51,4 +51,54 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout> --}}
+<!-- component -->
+<x-guest-layout>
+  <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt="Workflow">
+        <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+          {{__('Sign in to your account')}}
+        </h2>
+      </div>
+      <x-jet-validation-errors></x-jet-validation-errors>
+      <form class="mt-8"method="POST" action="{{ route('login') }}">
+        @csrf
+        {{-- <input type="hidden" name="remember" value="true"> --}}
+        <div class="rounded-md shadow-sm">
+          <div>
+            <input autofocus aria-label="{{__('E-Mail Address')}}" name="email" type="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="{{__('E-Mail Address')}}" value="{{old('email')}}">
+          </div>
+          <div class="-mt-px">
+            <input aria-label="{{__('Password')}}" name="password" type="password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="{{__('Password')}}">
+          </div>
+        </div>
+        <div class="mt-6 flex items-center justify-between">
+          <div class="flex items-center">
+            <input id="remember_me" name="remember" type="checkbox" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out">
+            <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">
+              {{__('Remember me')}}
+            </label>
+          </div>
+
+          <div class="text-sm leading-5">
+            <a href="{{ route('password.request') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+              {{__('Forgot your password?')}}
+            </a>
+          </div>
+        </div>
+        <div class="mt-6">
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+              </svg>
+            </span>
+            {{__('Login')}}
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </x-guest-layout>
