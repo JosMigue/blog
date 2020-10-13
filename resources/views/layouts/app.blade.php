e5b92ecbac6bf6d7db4b355c9a37c290e950d393
Francisco Vazquez <FranciscoIsco8@gmail.com>
2020-10-13 11:53:22 -0500

    Deleted css link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         
-        <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+       
 
         
 
