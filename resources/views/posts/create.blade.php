4171b3b1d9d47b3fa55f68e1ef21cd37039f222e
JosMigue <jojo_miguel20@hotmail.com>
2020-10-15 17:07:50 -0500

    Change view for show my posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+  @section('title', __('Add Post'))
     <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         {{ __('Add Post') }} 
--- resources/views/posts/myposts.blade.php
+++ resources/views/posts/myposts.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <x-slot name="slot">
       <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="w-auto lg:w-4/6 mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <table class="table-auto">
               <thead>
@@ -18,21 +18,22 @@
               </thead>
               <tbody>
                   @foreach($posts as $post)
-                <tr>
-                  <td class="border px-4 py-2"><img  class="object-contain h-48 w-full" src="/storage/cover_images/{{$post->image}}" alt="Sunset in the mountains"></td>
-                  <td class="border px-4 py-2">{{$post->title}}</td>
-                  <td class="border px-4 py-2">
-                    <form action="{{url('delete/' . $post->id) }}" method="POST">
-                      @csrf
-                      <button class="shadow bg-pink-500 hover:bg-pink-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-20 m-2" type="submit" >
-                        {{__('Delete')}}
-                      </button>
-                    </form>
-                    <a href="{{route('posts.edit',$post->id)}}" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-20 m-2" type="button">
-                      {{__('Edit')}}
-                    </a>
-                  </td>
-                </tr>
+                    <tr>
+                      <td class="border px-4 py-2"><img  class="object-contain h-32 w-full" src="/storage/cover_images/{{$post->image}}" alt="Sunset in the mountains"></td>
+                      <td class="border px-4 py-2">{{$post->title}}</td>
+                      <td class="border px-4 py-2">
+                        <form action="{{route('posts.destroy', $post->id)}}" method="POST">
+                          @csrf
+                          @method('DELETE')
+                          <button class="shadow bg-pink-500 hover:bg-pink-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-20 m-2" type="submit" >
+                            {{__('Delete')}}
+                          </button>
+                        </form>
+                        <a href="{{route('posts.edit',$post->id)}}" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-20 m-2" type="button">
+                          {{__('Edit')}}
+                        </a>
+                      </td>
+                    </tr>
                 @endforeach
               </tbody>
             </table>
