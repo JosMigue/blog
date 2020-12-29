f78bd129696d6e4de5840129abb2871c87343e37
JosMIgue <jojo_miguel20@hotmail.com>
2020-12-28 23:47:35 -0600

    Fix wrong route

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'LandingPageController@index');
-Route::get('/posts/{postName}', 'LandingPageController@show')->name('landing.show');
+Route::get('/posts/show/{postName}', 'LandingPageController@show')->name('landing.show');
 Route::get('/autors/{user}', 'LandingPageController@showAUthor')->name('landing.showAuthor');
-
-
-Route::get('/login', function () {
+Route::get('/login', function () { //Usar login controller para esta no callbacks
     return view('auth.login');
 })->name('login');
-
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard'); //usar un controlador para esta petición
 })->name('dashboard');
-
 Route::resource('users','UserController')->except('show');
-/* Route::middleware(['auth:sanctum', 'verified'])->get('/posts', 'PostController@index')->name('posts'); */
-/* Route::middleware(['auth:sanctum', 'verified'])->get('posts/create', function(){
-    return view('posts.create');
-})->name('create'); */
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/myposts', 'PostController@mypost')->name('myposts');
-
-
-
-
 Route::resource('posts', 'PostController');
-
-
